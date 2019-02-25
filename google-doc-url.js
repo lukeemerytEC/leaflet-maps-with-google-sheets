cfe7502f89e6280cf42cf84cbf171b7c14b3c357
lukeemerytEC <47980867+lukeemerytEC@users.noreply.github.com>
2019-02-25 16:12:28 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hkYmvHznpOLI5fqw0sufqX5-2YLFEl-_sxQAeXuGmcs/edit#gid=1196096370';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lyQkieu9XXxd8I4fP2znVI-e8XHlssOr5sf22uKSGfQ/edit#gid=0';
